bcca91a64f71cbf4115d0185a7b0c9b45257f21e
belhaddadmohamed <belhaddad.mohamed7@gmail.com>
2024-07-05 07:13:17 +0100

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,15 @@ use App\http\Controllers\StaticControler;
 use App\http\Controllers\ComputersController;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|
 */
+
+
+// Route::get => Defines a single route for a specific HTTP method (GET) and URL, mapping it to a specific method in a controller.
+
+// Route::resource => Defines multiple routes following RESTful conventions for a resource.
 
 Route::get('/', [StaticControler::class, 'index'])->name('home.index');     // The (.) means (/)
 
@@ -25,4 +25,6 @@ Route::get('/shop/{category?}/{item?}', [StaticControler::class, 'shop'])->name(
 
 Route::get('/store', [StaticControler::class, 'store'])->name('home.store');
 
-Route::resource('computers', ComputersController::class);
+Route::resource('computers', ComputersController::class);  //OR:  Route::get('computers/{computer}', [ComputersController::class, 'XXX'])->name('computers.XXX');
+
+
